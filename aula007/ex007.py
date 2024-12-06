187b9d50fdfe5ebfb19816b014ba832a9153262d
Geovanne Moreira <devgeovannemoreira@gmail.com>
2024-12-06 12:17:40 -0300

    reorganização de arquivos dentro de pastas

---
+++ aula007/ex007.py
@@ -0,0 +1,17 @@
+nome = str(input('Digite seu nome: '))
+print('Nome', nome)
+
+sobrenome = str(input('Digite seu sobrenome: '))
+print('Sobrenome', sobrenome)
+
+idade = int(input('Digite sua idade: '))
+print('Idade', idade)
+
+ano_de_nascimento = int(input('Digite o ano que você nasceu: '))
+
+maior_de_idade = idade >= 18
+print(f'Maior de idade? {maior_de_idade}')
+
+altura = float(input('Digite sua altura: '))
+print(f'Altura{altura:.2f}')
+
