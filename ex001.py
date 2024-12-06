6ebf76b9bff62d25a28029f8f501c9acceb331d2
Geovanne Moreira <devgeovannemoreira@gmail.com>
2024-12-06 11:06:05 -0300

    testando commit pelo github desktop

---
+++ ex001.py
@@ -0,0 +1,13 @@
+"""
+DocString
+E escrever o que eu
+quiser
+
+"""
+
+''' Usar 3' para escrever suas notas '''
+
+# O # antes da escrita permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
---
+++ ex002.py
@@ -0,0 +1,8 @@
+# Separador sep=''
+# Final da linha end='' empede que quebre a linha
+# que por utiliza o CRLF \n, \r que seria a quebra de linha
+
+print(12, 1, 34, 5, sep='-#-#-')
+print(12, 1, 34, 5)
+print('bola', 'cachorro', 'anão', 'bacalhau', end='___miaaaaaaaaaau________')
+print('bola', 'cachorro', 'anão', 'bacalhau', sep='----')
